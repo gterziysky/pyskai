a6f6e6d8216c9328bc659fec0c6fcb1107314eed
Galen Terziysky <8081700+gterziysky@users.noreply.github.com>
2020-08-05 15:16:14 +0300

    Add scripts to setup a Deep learning EC2 instance

---
+++ aws/create_ec2_spot_instance.py
@@ -0,0 +1,213 @@
+#!/usr/bin/env python
+import argparse
+import boto3
+import logging
+import os
+import re
+import time
+import urllib.request
+import base64
+
+
+def query_instance(instance_name):
+    existing_reservation = ec2.describe_instances(Filters=[{'Name': 'tag:Name', 'Values': [instance_name]}])
+    existing_reservation = existing_reservation['Reservations'][0]
+    instance = existing_reservation['Instances'][0]
+
+    return instance
+
+
+def get_machine_public_ip():
+    """
+    Get the public IP address of the current machine.
+
+    Returns:
+         str: The public IP address of the current machine.
+    """
+    external_ip = urllib.request.urlopen('https://ident.me').read().decode('utf8')
+    # https://www.regular-expressions.info/ip.html
+    regex = re.compile(pattern=r"^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$")
+    assert regex.match(string=external_ip) is not None, "Not a valid IP address. Please check the IP address lookup service"
+    return external_ip
+
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-in", "--instance_name", help="the name of the EC2 instance", default='dev-instance-1')
+    args = parser.parse_args()
+
+    instance_name = args.instance_name
+
+    m = re.search(pattern=r'.*?(\d{1,})$', string=instance_name)
+    if m is None:
+        err_msg = "Please provide a numeric suffix at the end of the instance name."
+        logger.error(err_msg)
+        raise ValueError(err_msg)
+
+    in_suffix = m.group(1)
+    # other input params
+
+    vol_name = 'dev-instance-ebs-%s' % in_suffix  # EBS vol name
+    iam_role_name = "EC2_dev_role"  # apply an IAM role to the instance
+
+    git_token = os.getenv('GIT_OAUTH_TOKEN', None)
+    if git_token is None:
+        err_msg = "Environment variable GIT_OAUTH_TOKEN is missing. " \
+                  "For more information see https://help.github.com/en/articles/git-automation-with-oauth-tokens"
+        logger.error(err_msg)
+        raise ValueError(err_msg)
+
+    # location of the bash script which sets up the machine
+    user_data_script = "./setup_ec2_instance.sh"
+
+    with open(user_data_script, 'r') as f:
+        txt = f.read()
+    # plug in the Git token and PG DB password
+
+    # TODO: going forward, those two should be read from the AWS parameter store
+    txt = txt.replace('GIT_PERSONAL_ACCESS_TOKEN_PLACEHOLDER', git_token)
+    user_data_script = txt.replace('PUBLIC_IP_ADDRESS_OF_COMPUTER_USED_TO_EXECUTE_THE_SCRIPT', get_machine_public_ip())
+
+    # remove all comments other than the shebang
+    # user_data_script = re.sub(pattern="#(?!\!).*?\n", repl="", string=user_data_script)
+    # remove all comments including the shebang
+    user_data_script = re.sub(pattern="#.*?\n", repl="", string=user_data_script)
+    # 1. Execute the script within a sudo command in order to run as the non-root user (by default cloud init scripts are
+    # run as root).
+    # 2. Enclose the commands within single quotes ' ' and prefix with a $-sign. The $ allows us to have single quotes
+    # within the commands provided they are escaped, i.e. \'. This is the only way to change the postgres user password.
+    # https://stackoverflow.com/questions/8254120/how-to-escape-a-single-quote-in-single-quote-string-in-bash/26165123#answer-8254156
+    user_data_script = "#!/bin/bash\nsudo -H -u ubuntu /bin/bash -c $'%s'" % user_data_script
+
+    # Standard Base64 Encoding
+    encodedBytes = base64.b64encode(user_data_script.encode("utf-8"))
+    # Base64 encoded UserData script
+    user_data_script = str(encodedBytes, "utf-8")
+
+    ec2 = boto3.client('ec2')
+    # Ubuntu 20.04 LTS image
+    # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ec2.html#EC2.Client.run_instances
+    # datetime.datetime(2020, 7, 24, 12, 13, 14)
+
+    # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ec2.html#EC2.Client.request_spot_instances
+    # TODO: get the spot price from the API
+    #       https://aws.amazon.com/about-aws/whats-new/2017/04/use-the-enhanced-aws-price-list-api-to-access-aws-service-and-region-specific-product-and-pricing-information/
+    #       https://docs.aws.amazon.com/awsaccountbilling/latest/aboutv2/price-changes.html
+    reservation = ec2.request_spot_instances(
+        DryRun=False,
+        SpotPrice='0.20',
+        Type='one-time',
+        InstanceCount=1,
+        # ValidFrom=datetime.datetime(),
+        # ValidUntil=,
+        # TagSpecifications=[
+        #     {'ResourceType': 'instance',
+        #      'Tags': [
+        #         {
+        #             'Key': 'Name',
+        #             'Value': instance_name
+        #         }
+        #     ]
+        #     }
+        # ],
+        LaunchSpecification={
+            # ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-20200716 - ami-079024c517d22af5b
+            'ImageId': 'ami-079024c517d22af5b',
+            'KeyName': 'GalenMBP',
+            'SecurityGroups': ['ssh-access-limit'],
+            'InstanceType': 'g4dn.xlarge',
+            'Placement': {
+                'AvailabilityZone': 'eu-central-1a',
+            },
+            'IamInstanceProfile': {'Name': iam_role_name},
+            'UserData': user_data_script,
+            'BlockDeviceMappings': [
+                {
+                    'DeviceName': "/dev/sda1",
+                    'Ebs': {
+                        # 'SnapshotId': 'snap-f70deff0',
+                        'VolumeSize': 20,
+                        'DeleteOnTermination': True,
+                        'VolumeType': 'gp2',
+                        # 'Iops': 300,
+                        # 'Encrypted': False
+                    },
+                },
+            ],
+            # 'EbsOptimized': True,
+
+            # 'Monitoring': {
+            #     'Enabled': True
+            # },
+            'SecurityGroupIds': [
+                'sg-0aca069fbc378e459',
+            ],
+        }
+    )
+
+    # reservation['SpotInstanceRequests'][0]['State']
+    # 'SpotInstanceRequestId'
+    # instance = reservation['Instances'][0]
+    spot_inst_req_id = reservation['SpotInstanceRequests'][0]['SpotInstanceRequestId']
+
+    res = ec2.describe_spot_instance_requests(
+        DryRun=False,
+        SpotInstanceRequestIds=[
+            spot_inst_req_id
+        ]
+    )
+
+    while 'fulfilled' != res['SpotInstanceRequests'][0]['Status']['Code']:
+        res = ec2.describe_spot_instance_requests(
+            DryRun=False,
+            SpotInstanceRequestIds=[
+                spot_inst_req_id
+            ]
+        )
+        logger.info("Request Status last updated %s\nStatus code: %s\nStatus message: %s\nRequest created at %s\nRequest expires at %s",
+                    res['SpotInstanceRequests'][0]['Status']['UpdateTime'].strftime("%Y-%m-%d %H:%M:%S UTC"),
+                    res['SpotInstanceRequests'][0]['Status']['Code'],
+                    res['SpotInstanceRequests'][0]['Status']['Message'],
+                    res['SpotInstanceRequests'][0]['CreateTime'].strftime("%Y-%m-%d %H:%M:%S UTC"),
+                    res['SpotInstanceRequests'][0]['ValidUntil'].strftime("%Y-%m-%d %H:%M:%S UTC"))
+        time.sleep(10)
+
+    if 'InstanceId' in res['SpotInstanceRequests'][0].keys():
+        # at this point we already have an instance ID
+        instance_id = res['SpotInstanceRequests'][0]['InstanceId']
+    else:
+        logger.info("Instance reservation failed.")
+    # create a human-friendly name for the instance
+    # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ec2.html#EC2.ServiceResource.create_tags
+    # tag the image
+    ct_res = ec2.create_tags(Resources=[instance_id],
+                             Tags=[{'Key': 'Name', 'Value': instance_name}])
+
+    # # wait for the VolumeId to become available
+    # while len(instance['BlockDeviceMappings']) == 0:
+    #     logger.info("No device is attached to the instance yet. Waiting for the instance to become available...")
+    #     time.sleep(10)
+    #     instance = query_instance(instance_name=instance_name)
+    #
+    # vol_id = instance['BlockDeviceMappings'][0]['Ebs']['VolumeId']
+    #
+    # # create a human-friendly name for the volume
+    # res = ec2.create_tags(Resources=[vol_id],
+    #                       Tags=[{'Key': 'Name', 'Value': vol_name}])
+
+    # wait until the EC2 instance is up and running
+    instance = query_instance(instance_name=instance_name)
+    # while 'PublicDnsName' not in instance.keys() or instance['PublicDnsName'] == '':
+    while instance['State']['Name'] == 'running':
+        logger.info("Instance state: %s. Waiting for the instance to become available...", instance['State']['Name'])
+        time.sleep(10)
+        instance = query_instance(instance_name=instance_name)
+
+    # at this point the public DNS should already be available
+    logger.info("Instance state: %s; Public DNS: %s", instance['State']['Name'], instance['PublicDnsName'])
+    logger.info("To ssh into the machine do:\nssh -i ~/.ssh/id_rsa ubuntu@%s", instance['PublicDnsName'])
---
+++ aws/setup_ec2_instance.sh
@@ -0,0 +1,152 @@
+#!/bin/bash
+
+# make sure we perform this script for a non-sudo user
+# the script currently requires about 10.5 mins to complete (on a GPU EC2 instance)
+
+DISTRIBUTION="miniconda"
+# DISTRIBUTION="anaconda"
+if [ $DISTRIBUTION == "miniconda" ]; then
+    # miniconda specific variables
+    CONDA_INSTALL_SCRIPT="Miniconda3-latest-Linux-x86_64.sh"
+    CONDA_DIR_NAME="miniconda"
+    CONDA_URL="https://repo.anaconda.com/miniconda"
+else
+    # anaconda specific variables
+    CONDA_INSTALL_SCRIPT="Anaconda3-5.3.1-Linux-x86_64.sh"
+    CONDA_DIR_NAME="anaconda"
+    CONDA_URL="https://repo.anaconda.com/archive"
+fi
+
+CONDA_ENV="rlenv"
+REPO_NAME="pyskai"
+GIT_OAUTH_TOKEN="GIT_PERSONAL_ACCESS_TOKEN_PLACEHOLDER"
+PUBLIC_IP_ADDRESS="PUBLIC_IP_ADDRESS_OF_COMPUTER_USED_TO_EXECUTE_THE_SCRIPT"
+
+# temp fix of a bug in Anaconda which has been described here:
+# https://github.com/conda/conda/issues/7267#issuecomment-387329419
+unset SUDO_UID
+
+# install anaconda
+# https://www.digitalocean.com/community/tutorials/how-to-install-anaconda-on-ubuntu-18-04-quickstart
+cd /tmp || exit
+curl -O $CONDA_URL/$CONDA_INSTALL_SCRIPT
+
+# silent anaconda install
+# https://docs.anaconda.com/anaconda/install/silent-mode/
+bash $CONDA_INSTALL_SCRIPT -b -p $HOME/$CONDA_DIR_NAME
+
+rm $CONDA_INSTALL_SCRIPT
+
+# https://docs.anaconda.com/anaconda/install/silent-mode/#linux-macos
+# in each new bash session, before using conda, set the PATH and run
+export PATH="$HOME/$CONDA_DIR_NAME/bin:$PATH"
+source $HOME/$CONDA_DIR_NAME/bin/activate 
+
+# init conda for BASH for future use (this modifies ~/.bashrc)
+echo export PATH="$HOME/$CONDA_DIR_NAME/bin:"\'$PATH\' >> $HOME/.bashrc
+echo source $HOME/$CONDA_DIR_NAME/bin/activate  >> $HOME/.bashrc
+
+# conda init bash > /dev/null
+
+# to update anaconda follow the guide below
+# https://www.digitalocean.com/community/tutorials/how-to-install-the-anaconda-python-distribution-on-ubuntu-18-04#updating-anaconda
+
+# to uninstall anaconda follow the guide below
+# https://www.digitalocean.com/community/tutorials/how-to-install-the-anaconda-python-distribution-on-ubuntu-18-04#uninstalling-anaconda
+
+# update the conda package in the base conda env
+conda update -n base -c defaults conda -y
+
+if [ $DISTRIBUTION == "miniconda" ]; then
+    # update all packages in the base conda env
+    conda update --all -y
+else
+    # update the anaconda packages in the base conda env
+    conda update -y anaconda
+fi
+
+# switch to home
+cd $HOME || exit
+
+# install postgres
+sudo apt-get update
+sudo apt-get -y install postgresql postgresql-contrib
+
+# set up new env in silent mode and install pandas
+# https://docs.conda.io/projects/conda/en/latest/user-guide/tasks/manage-environments.html#creating-an-environment-with-commands
+# TODO: update pandas to 0.25 when it is released so as to be able to use pd.DataFrame.query on columns with spaces om their names
+conda create --name $CONDA_ENV --channel conda-forge --yes python=3.6 pandas=0.24.2 notebook=5.7.8 boto3=1.9.134 scikit-learn=0.20.3
+
+# activate the new environment
+conda activate $CONDA_ENV
+# activate the new environment automatically in each subsequent shell session
+echo conda activate $CONDA_ENV >> $HOME/.bashrc
+
+# ========
+conda install -y -c conda-forge numpy=1.14.2 scipy=1.0.1 \
+gym=0.17.2 tensorboard=1.13.1 tensorflow=1.13.1 \
+tensorboardx=2.1 matplotlib=3.1.0 pandas=0.24.2
+
+conda install -y -c akode atari-py=0.1.1
+
+pip install opencv-python==3.4.0.12 ptan==0.3
+# ========
+
+# see if there is a CUDA capable GPU
+# https://varhowto.com/install-pytorch-ubuntu-20-04/#Step_2_%E2%80%94_Install_NVIDIA_Linux_driver
+# better - https://linuxconfig.org/how-to-install-the-nvidia-drivers-on-ubuntu-20-04-focal-fossa-linux
+sudo add-apt-repository -y ppa:graphics-drivers/ppa
+sudo apt-get install -y ubuntu-drivers-common
+# ubuntu-drivers devices
+# sudo ubuntu-drivers autoinstall
+sudo apt-get install -y nvidia-driver-450
+
+# https://docs.nvidia.com/cuda/cuda-installation-guide-linux/index.html#verify-you-have-cuda-enabled-system
+# GPU=$(lspci | grep -i -e "VGA" | grep -i -e "NVIDIA" | head -n1)
+GPU=$(lspci | grep -i -e "3D controller" | grep -i -e "NVIDIA" | head -n1)
+# 3D controller
+# for testing:
+# GPU=$(lspci | grep -i -e "abvg")
+
+if [ -z "$GPU" ]; then
+        echo "installing PyTorch on CPU"
+	$HOME/$CONDA_DIR_NAME/bin/conda install -c pytorch -y pytorch-cpu torchvision-cpu
+else
+        echo "installing PyTorch with CUDA"
+	$HOME/$CONDA_DIR_NAME/bin/conda install  -c pytorch -y pytorch torchvision cudatoolkit=10.2
+fi
+
+# create a folder to hold the code
+mkdir $HOME/repos
+cd $HOME/repos || exit
+
+# download the code repositories
+if [ -n "$GIT_OAUTH_TOKEN" ]; then
+	echo "cloning code repositories..."
+	# clone the necessary code
+	git clone https://$GIT_OAUTH_TOKEN:x-oauth-basic@github.com/gterziysky/$REPO_NAME.git
+else
+	echo "$GIT_OAUTH_TOKEN is empty."
+	echo "To set up a personal access token,"
+	echo "please follow the guide at: https://help.github.com/en/articles/git-automation-with-oauth-tokens"	
+fi
+
+# Set up postfix to read mail from cron:
+# https://serverfault.com/questions/143968/automate-the-installation-of-postfix-on-ubuntu
+sudo debconf-set-selections <<< "postfix postfix/mailname string localhost"
+sudo debconf-set-selections <<< "postfix postfix/main_mailer_type string \'Local only\'"
+sudo apt-get -y install postfix
+
+# create a startup script with instructions
+touch startup_script
+chmod a+x startup_script
+
+echo export PATH="$HOME/$CONDA_DIR_NAME/bin:"$PATH >> $HOME/startup_script
+echo . $HOME/$CONDA_DIR_NAME/bin/activate  >> $HOME/startup_script
+echo conda activate $CONDA_ENV  >> $HOME/startup_script
+
+## Now on the server, run:
+# jupyter notebook --no-browser --port 1234
+## and note the command output
+## then open the notebook locally:
+# ssh -i ~/.ssh/id_rsa -NL 1234:localhost:1234 ubuntu@ec2_instance_dns
